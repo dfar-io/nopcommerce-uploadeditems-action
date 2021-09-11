a4b258677b6f283f71acbdb0730d75e4138ef0a1
Dave Farinelli <d@dfar.io>
2021-09-11 04:47:24 -0400

    More error logging

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ try {
     const isDirectory = fs.lstatSync(file).isDirectory() && !file.includes('.git')
 
     if (isDirectory) {
+      console.log(`processing directory: ${file}`);
       const jsonFile = `${file}/${pluginName}/plugin.json`;
       if(!fs.existsSync(jsonFile)) {
         throw new Error("plugin.json not found at ${jsonFile}");
@@ -28,6 +29,10 @@ try {
       entries.push(uploadedItemsEntry);
     }
   });
+  
+  if (entries.length <= 0) {
+    throw new Error("No uploadedItems entries returned.");
+  }
 
   fs.writeFileSync('uploadedItems.json', JSON.stringify(entries, null, '\t'));
 } catch (error) {
